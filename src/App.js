56cda3f130b8d90cc0d7a15644cd7d3a534dc372
Aritra Mondal <mondalaritra002@gmail.com>
2021-08-09 11:21:08 +0530

    Staging

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Articles from './views/Articles';
 
 import Navbar from './components/Navbar';
 
-import {Route, Link} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 
 function App() {
   return (
