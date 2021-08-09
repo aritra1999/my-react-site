7b70a942fd7e94c603d307a49f78ffdc17acb13c
Aritra Mondal <mondalaritra002@gmail.com>
2021-08-09 11:19:00 +0530

    Staging

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,11 +21,11 @@ function Navbar(){
                         <Link className="block" to="/resume">Resume</Link>
                     </div>
                     <div className="flex space-x-8 flex-row lg:text-2xl md:text-2xl sm:text-xl">
-                        <a target="_blank" href="mailto:1806290@kiit.ac.in"><i className="fas fa-envelope"></i></a>
-                        <a target="_blank" href="https://twitter.com/aritramondal199"><i className="fab fa-twitter"></i></a>
-                        <a target="_blank" href="https://www.instagram.com/__aritra_mondal__/"><i className="fab fa-instagram"></i></a>
-                        <a target="_blank" href="https://www.linkedin.com/in/aritra1999/"><i className="fab fa-linkedin"></i></a>
-                        <a target="_blank" href="https://www.youtube.com/channel/UCaOZb2OH84nCezEt9gnKy-Q"><i className="fab fa-youtube"></i></a>
+                        <Link to="mailto:1806290@kiit.ac.in"><i className="fas fa-envelope"></i></Link>
+                        <Link to="https://twitter.com/aritramondal199"><i className="fab fa-twitter"></i></Link>
+                        <Link to="https://www.instagram.com/__aritra_mondal__/"><i className="fab fa-instagram"></i></Link>
+                        <Link to="https://www.linkedin.com/in/aritra1999/"><i className="fab fa-linkedin"></i></Link>
+                        <Link to="https://www.youtube.com/channel/UCaOZb2OH84nCezEt9gnKy-Q"><i className="fab fa-youtube"></i></Link>
                     </div>
                 </div>
             </div>
